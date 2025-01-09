66baacbf4616155e5b0a68c354640e7b8e04c183
jade236 <janejose1402@gmail.com>
2025-01-09 19:54:15 +0530

    Now better

--- first.py
+++ first.py
@@ -1,8 +1,6 @@
 print('hey im jane')
 print('Reloading..')
-<<<<<<< HEAD
 print('For the prod')
-=======
 print('For the dev')
 print('Making changes to pull')
->>>>>>> cfd5c847bae01e9fec500762d93fd4a6be376b49
+print('yo')
